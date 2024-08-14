dd5dd33f50993daf9bf6c71eb58818f76aa78808
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-14 21:41:17 +0200

    rimosse dipendenze ImageConverter

--- src/ai_model.py
+++ src/ai_model.py
@@ -1,5 +1,4 @@
 from TextConverter import UniversalTranslator
-from ImageConverter import better_image
 
 def get_converter(conversion_type):
     if conversion_type == "text_en_to_fr":
@@ -8,7 +7,5 @@ def get_converter(conversion_type):
         return UniversalTranslator("en", "de")
     elif conversion_type == "text_it_to_en":
         return UniversalTranslator("it", "en")
-    elif conversion_type == "better_image":
-        return better_image()
     else:
         return None
