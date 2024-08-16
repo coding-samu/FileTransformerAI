d334161cf2154c730d8db68162a5f6cf3e279cdc
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-16 17:31:33 +0200

    creata funzione save_file_png in riferimento all'issue #46

--- src/utils/utils_file.py
+++ src/utils/utils_file.py
@@ -85,3 +85,15 @@ def load_file_png(file_path):
     except Exception as e:
         print(f"Errore durante il caricamento del file {file_path}: {e}")
         return None
+    
+def save_file_png(data, file_path):
+    # verifica che l'estensione del file sia .png
+    if not file_path.endswith('.png'):
+        print(f"Estensione del file {file_path} non supportata.")
+        return None
+    try:
+        with open(file_path, 'wb') as file:
+            file.write(data)
+    except Exception as e:
+        print(f"Errore durante il salvataggio del file {file_path}: {e}")
+        return None
