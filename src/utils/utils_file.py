55598895023a509d242421b8cf1a57f64e58201a
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-16 17:23:44 +0200

    creata funzione save_file_jpg in riferimento all'issue #45

--- src/utils/utils_file.py
+++ src/utils/utils_file.py
@@ -61,3 +61,15 @@ def load_file_jpg(file_path):
     except Exception as e:
         print(f"Errore durante il caricamento del file {file_path}: {e}")
         return None
+    
+def save_file_jpg(data, file_path):
+    # verifica che l'estensione del file sia .jpg
+    if not file_path.endswith('.jpg'):
+        print(f"Estensione del file {file_path} non supportata.")
+        return None
+    try:
+        with open(file_path, 'wb') as file:
+            file.write(data)
+    except Exception as e:
+        print(f"Errore durante il salvataggio del file {file_path}: {e}")
+        return None
