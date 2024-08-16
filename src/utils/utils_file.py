1e3428efb519069ed9fe31500e7a6f0dbe8f5559
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-16 12:36:48 +0200

    creata funzione save_file_pdf in riferimento all'issue #44

--- src/utils/utils_file.py
+++ src/utils/utils_file.py
@@ -35,3 +35,18 @@ def load_file_pdf(file_path):
     except Exception as e:
         print(f"Errore durante il caricamento del file {file_path}: {e}")
         return None
+    
+def save_file_pdf(data, file_path):
+    # verifica che l'estensione del file sia .pdf
+    if not file_path.endswith('.pdf'):
+        print(f"Estensione del file {file_path} non supportata.")
+        return None
+    try:
+        writer = PdfWriter()
+        for page in data.pages:
+            writer.add_page(page)
+        with open(file_path, 'wb') as file:
+            writer.write(file)
+    except Exception as e:
+        print(f"Errore durante il salvataggio del file {file_path}: {e}")
+        return None
