7fffb03aa3175231e2133ff11036ed66814551d4
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-21 16:01:45 +0200

    implementata struttura delle classi per la conversione dei file di tipo docx, in riferimento agli issue #15, #16, #17, #18, #19, #20, #21

---
+++ src/ai_model/ai_model_docx.py
@@ -0,0 +1,20 @@
+class DOCXPDF:
+    pass # TODO: implementare la conversione da DOCX a PDF
+
+class DOCXJPG:
+    pass # TODO: implementare la conversione da DOCX a JPG
+
+class DOCXPNG:
+    pass # TODO: implementare la conversione da DOCX a PNG
+
+class DOCXXLSX:
+    pass # TODO: implementare la conversione da DOCX a XLSX
+
+class DOCXTXT:
+    pass # TODO: implementare la conversione da DOCX a TXT
+
+class DOCXSVG:
+    pass # TODO: implementare la conversione da DOCX a SVG
+
+class DOCXTXTSummary:
+    pass # TODO: implementare la conversione da DOCX a TXT con riassunto
