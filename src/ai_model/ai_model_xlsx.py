03b6cd96defbd90fc527c3f2e31aef9e5284c065
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-23 11:10:51 +0200

    implementata la struttura delle classi per gli issue #22, #23, #24, #25, #26, #27

---
+++ src/ai_model/ai_model_xlsx.py
@@ -0,0 +1,19 @@
+from utils.utils_file import save_file_docx, save_file_txt, save_file_pdf
+
+class XLSXPDF:
+    pass
+
+class XLSXJPG:
+    pass
+
+class XLSXPNG:
+    pass
+
+class XLSXDOCX:
+    pass
+
+class XLSXTXT:
+    pass
+
+class XLSXSVG:
+    pass
